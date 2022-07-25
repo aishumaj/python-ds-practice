d4a9392206bb73f388971a0099b9366224afeaa4
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-07-25 16:09:30 -0700

    first seven functions complete

--- 07_multiple_letter_count.py
+++ 07_multiple_letter_count.py
@@ -7,3 +7,11 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    dict_of_letters ={}
+
+    for letter in phrase:
+        dict_of_letters[letter] = phrase.count(letter)
+
+    return dict_of_letters
+
+
